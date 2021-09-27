9fe058e62e16de1b914766f356a3944917e15166
Esthar077 <87311303+Esthar077@users.noreply.github.com>
2021-09-27 15:52:01 +0530

    Update dns-client.py

--- dns-client.py
+++ dns-client.py
@@ -8,10 +8,11 @@ Created on Tue Aug 31 16:20:40 2021
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.43.154", 1234))
-
+s.connect((socket.gethostname(), 1234))
 req_u = s.recv(1024)
+//getting url from client
 url = input(str(req_u.decode("utf-8")))
 s.send(bytes(url,"utf-8"))
 rep = s.recv(1024)
+//printing response
 print(rep.decode("utf-8"))
